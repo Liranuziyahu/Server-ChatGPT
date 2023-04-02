297e1345fca1d6af56cbeaef2728deb73f19ca79
Liran Uziyahu <liranuzistud@gmail.com>
2023-04-02 20:59:44 +0300

    with request

--- Controllers/translate.js
+++ Controllers/translate.js
@@ -9,13 +9,14 @@ const data = {
     "model": "text-davinci-003",
     "prompt": ``,
     "max_tokens":500,
-    "temperature": 0.5
+    "temperature": 0.5,
   }
+  
   exports.translate = async (req, res) => {
-    data.prompt = `${req.body.text}`;
-    console.log(data);
+    data.prompt = `I'm looking for a vacation, for ${req.body.people} friends, every one has a budget of ${req.body.budget}$, we are looking for a vacation to  ${req.body.mainland} , give me also details about attraction there`;
     axios.post('https://api.openai.com/v1/completions', data , config)
     .then(function (response) {
+      console.log(response);
         res.status(200).send({message:response.data.choices[0].text})
     })
     .catch(function (error) {
